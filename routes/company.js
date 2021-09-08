db8ee742d97c04d68bde5d81025291f102599590
niraj-snappymob <niraj.jani@snappymob.com>
2021-09-09 00:31:07 +0800

    Add API for location list, skill list and statelist

--- routes/company.js
+++ routes/company.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import {createCompany, getCompany, updateCompany, deleteCompany,updateLocation,getLocation , migration , getActionPlan} from '../controllers/company.js';
+import {createCompany, getCompany, updateCompany, deleteCompany,updateLocation,getLocation , migration , getLocationList, getSkillList} from '../controllers/company.js';
 
 const router = express.Router();
 
@@ -10,6 +10,8 @@ router.post('/fetchLocation',getLocation);
 router.post('/updateCompany', updateCompany);
 router.post('/updateLocation', updateLocation);
 router.post('/deleteCompany',  deleteCompany);
+router.get('/fetchLocationList',getLocationList);
+router.get('/fetchSkillList',getSkillList);
 
 router.get('/test', function (req, res) {
   res.send('test home page');
