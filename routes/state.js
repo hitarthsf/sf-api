1ff4fde6cdfe54b017225995e8eecf52139161ea
niraj-snappymob <niraj.jani@snappymob.com>
2021-09-09 00:31:07 +0800

    Add API for location list, skill list and statelist

---
+++ routes/state.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+import {getStateList} from '../controllers/StateController.js';
+
+const router = express.Router();
+
+router.get('/fetchStateList',getStateList);
+export default router;
