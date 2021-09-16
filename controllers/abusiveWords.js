d268f09347c0bbc89c6d6703037fee2bbf1ba7f0
niraj-snappymob <niraj.jani@snappymob.com>
2021-09-16 23:49:08 +0800

    basic API changes

--- controllers/abusiveWords.js
+++ controllers/abusiveWords.js
@@ -1,36 +1,33 @@
-
 import CompanyData from '../models/CompanyData.js';
+import {Messages, MesssageProvider} from "../core/index.js";
 
 
 export const getAbusiveWords = async (req,res) => {
-    //res.send('THIS GOOD');
     const  id  = req.body._id;
-    res.send(id);
+    const companyId = req.query.company_id;
+    if (!companyId) {
+        res.status(500).json({message : MesssageProvider.messageByKey(Messages.KEYS.ID_NOT_FOUND)});
+    }
+    console.log('companyId', companyId);
     try {
       //  const AllCompany = await CompanyData.find({"_id":id});
       // make it dynamic
-      const AllCompany = await CompanyData.find(  {"_id":"6111149b961aa70d06fe58f3"} ,{ $lookup:
-           {
-             from: 'location',
-             localField: 'ObjectId(location_id)',
-             foreignField: 'ObjectId(_id)',
-             as: 'location_name'
-           }});
-      console.log(AllCompany);
-        res.status(200).json(AllCompany);
+      const company = await CompanyData.findOne(  {"_id":companyId}, {abusive_word: 1});
+        res.status(200).json(company.abusive_word ? company.abusive_word : []);
     } catch (error) {
         res.status(404).json({message : error.message});
     }
 }
 
 export const createAbusiveWords = async(req,res) => {
-
-   console.log(req.body)
-    
+    const companyId = req.body.company_id;
+    if (!companyId) {
+        res.status(500).json({message : MesssageProvider.messageByKey(Messages.KEYS.ID_NOT_FOUND)});
+    }
     var objFriends = { word:req.body.word};
     // make it dynamic
     CompanyData.findOneAndUpdate(
-       { _id: "6111149b961aa70d06fe58f3" }, 
+       { _id: companyId },
        { $push: { abusive_word: objFriends  } },
       function (error, success) {
             if (error) {
@@ -41,18 +38,22 @@ export const createAbusiveWords = async(req,res) => {
                 res.send(success)
             }
         });
-    
+
 }
 
 export const deleteAbusiveWords = async(req,res) => {
 
    const  id  = req.body._id;
-    
+    const companyId = req.body.company_id;
+    if (!companyId) {
+        res.status(500).json({message : MesssageProvider.messageByKey(Messages.KEYS.ID_NOT_FOUND)});
+    }
+
     // const Company = await CompanyData.findOneAndUpdate({"_id":"6111149b961aa70d06fe58f1"});
     // CompanyData.update( {"_id":"6111149b961aa70d06fe58f1"}, { $pull: { votes: { $gte: 6 } } } )
     // make it dynamic
     await CompanyData.updateOne(
-       { _id: "6111149b961aa70d06fe58ed" }, 
+       { _id: companyId },
          { $pull: { abusive_word: { _id: id } } } ,
          { multi: true },
       function (error, success) {
@@ -64,5 +65,5 @@ export const deleteAbusiveWords = async(req,res) => {
                 // res.send(success)
             }
         });
-    
+
 }
