d590016eacc86607544c10af098f44d3e39a5939
hitarth <hitarth.rc@gmail.com>
2021-11-17 14:48:18 +0530

    count adde

--- routes/ratings.js
+++ routes/ratings.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
-import {createRating, fetchRating} from '../controllers/ratings.js';
+import {createRating, fetchRating , singleRating} from '../controllers/ratings.js';
 
 const router = express.Router();
 
 router.post('/create', createRating);
 router.post('/fetch', fetchRating);
-
+router.get('/singleRating', singleRating);
 export default router;
