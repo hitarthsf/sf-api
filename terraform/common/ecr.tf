c4f2346d52397c98772c237f7ef722a834b5d35c
Alan M <alan@nexusfinance.app>
2022-01-03 23:59:42 +0000

    fixed terraform common

--- terraform/common/ecr.tf
+++ terraform/common/ecr.tf
@@ -1,3 +1,10 @@
-resource "aws_ecr_repository" "rails_blog_example" {
-  name = "rails-blog-example"
+resource "aws_ecr_repository" "sf_api" {
+  name = "sf-api"
+  encryption_configuration {
+    encryption_type = "KMS"
+    kms_key         = "arn:aws:kms:eu-west-2:068531097348:key/17a22730-5428-4a81-a4ef-2564080501d2"
+  }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
--- terraform/common/outputs.tf
+++ terraform/common/outputs.tf
@@ -6,6 +6,6 @@ output "ecs_application_autoscaling_role_arn" {
   value = aws_iam_service_linked_role.ecs_application_autoscaling.arn
 }
 
-output "ecr_rails_blog_example_repository_url" {
-  value = aws_ecr_repository.rails_blog_example.repository_url
+output "sf_api_repository_url" {
+  value = aws_ecr_repository.sf_api.repository_url
 }
