593e8afc291d57772a8b4b253a25ea2b33cc7ccc
hitarth <hitarth.rc@gmail.com>
2022-01-13 13:37:01 +0530

    all comapy

--- controllers/cronController.js
+++ controllers/cronController.js
@@ -1,8 +1,27 @@
 import UsersData from "../models/UsersData.js";
+import CompanyData from "../models/CompanyData.js";
 
 export const adminMail = async (req, res) => {
 
     var id = req.query.id;
-    var userData = await UsersData.find({});
+    var userData = await UsersData.find({"type":"area_manager"});
+    
+    var locationDataMap = new Map();
+    var company_id  = [] ;
+    
+    var company = await CompanyData.find({});
+    await company.map(async (singleCompany) => {
+        var location_id = [] ;
+         await singleCompany.location.map( async (singelLocation) => {
+            location_id.push(singelLocation._id);
+        })
+        locationDataMap.set(singleCompany._id,location_id);
+    })
+
+    
+    var userData = await UsersData.find({type:"area_manager"});
+    userData.find.map((user) => {
+        var userLocation  = locationDataMap.get(user.company_id);
+    })
     res.status(200).json({ data: userData, message: "Admin mail cron" });
 }
