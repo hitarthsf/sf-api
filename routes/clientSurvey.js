810bbd3b74d506625f096d30e697a2128fe327bb
hitarth <hitarth.rc@gmail.com>
2022-01-17 09:14:39 +0530

    clean up

--- routes/clientSurvey.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { addClientSurvey } from "../controllers/clientSurveyController.js";
-
-const router = express.Router();
-
-router.post("/addClientSurvey", addClientSurvey);
-
-export default router;
--- routes/state.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-import { getStateList } from "../controllers/stateController.js";
-
-const router = express.Router();
-
-router.get("/fetchStateList", getStateList);
-export default router;
