914ac5328e045ca88e1f7b012c7acfd484697ee8
Vishal Soni <vstechnology2019@gmail.com>
2022-01-20 13:38:37 +0530

    Report PDF

--- controllers/generalController.js
+++ controllers/generalController.js
@@ -26,7 +26,16 @@ export const generateReportPdf = async (req, res) => {
   // or a .pdf file
   await page.pdf({
     format: 'A4',
-    path: process.cwd()+'/my-report.pdf'
+    path: process.cwd()+'/my-report.pdf',
+    displayHeaderFooter: true,
+    headerTemplate: '<div style="padding-right:15px;font-weight:bold;-webkit-print-color-adjust: exact; width:100%"><p style="font-size:12px; color: black; float:right;">MONTHLY DATA REPORT<br><span style="font-size:10px;--tw-text-opacity:1;color:rgba(110,37,127,var(--tw-text-opacity));font-weight:bold;">September 2021</span></p></div>',
+    footerTemplate: '<div style="font-size: 5px; margin-left: 10px;" class="pageNumber"></div>',
+    margin: {
+      bottom: 70, // minimum required for footer msg to display
+      // left: 25,
+      // right: 25,
+      top: 72,
+    }
   })
 
   // close the browser
