8ca5345f7951abe90daf1957acbb246ea29e3b71
hitarth <hitarth.rc@gmail.com>
2022-01-25 10:02:33 +0530

    sync

---
+++ routes/hubSpot.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+import {
+  sendMail,
+  getHubspotCompanies,
+  createContact,
+  assignContactToCompany
+} from "../controllers/hubspotController.js";
+
+const router = express.Router();
+
+router.post("/sendMail", sendMail);
+router.get("/getHubspotCompanies", getHubspotCompanies);
+router.post("/createContact", createContact);
+router.put("/assignContactToCompany", assignContactToCompany);
+
+export default router;
